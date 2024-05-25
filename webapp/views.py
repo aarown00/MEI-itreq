500aeca94816b8a39619121f03655eb49ffb1e43
Aaron Gerald T. Martillano <aarown.00@gmail.com>
2024-05-25 19:07:47 +0800

    'login'

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+	# Check to see if logging in
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		# Authenticate
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, "You Have Been Successfully Logged In!")
+			return redirect('home')
+		else:
+			messages.error(request, "Account credentials is invalid. Please try again or register.")
+			return redirect('home')
+	else:
+		return render(request, 'home.html')
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You Have Been Successfully Logged Out!")
+    return redirect('home')
+
+
+def register_user(request):
+    return render(request, 'register.html')
