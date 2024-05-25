ec20a35aaa95a11c7d6e18374572920c2c977975
Aaron Gerald T. Martillano <aarown.00@gmail.com>
2024-05-26 02:05:39 +0800

    'register'

--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from .forms import RegisterForm
 
 def home(request):
 	# Check to see if logging in
@@ -28,4 +28,21 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, 'register.html')
+	if request.method == 'POST':
+		form = RegisterForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+		
+		
+	else:
+		form = RegisterForm()
+		return render(request, 'register.html', {'form':form})
+
+	return render(request, 'register.html', {'form':form})
